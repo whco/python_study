54ae03cae18ae7f832712fdf7163762cfb7de260
whco <einsteized@gmail.com>
2022-09-03 19:50:56 +0900

    Initial commit

---
+++ algorithm/programmers/level1/성격_유형_검사하기.py
@@ -0,0 +1,21 @@
+def solution(survey, choices):
+    #rt 몇 점인지
+    answer = ''
+    scores = dict()
+    for i in range(len(survey)):
+        if survey[i] not in scores:
+            scores[survey[i]] = 0
+        scores[survey[i]] += choices[i] - 4
+    def verify_type(type):
+        score = scores.get(type, 0) - scores.get(type[::-1], 0)
+        if score == 0:
+            return min(type[0], type[1])
+        return type[0] if score < 0 else type[1]
+    types = ["RT", "FC", "MJ", "AN"]
+    for type in types:
+        answer += verify_type(type)
+    return answer
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]))
