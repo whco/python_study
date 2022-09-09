3ad553008a6bd9bd9b01a8809ca59d26b49dedbb
whco <einsteized@gmail.com>
2022-09-09 19:56:45 +0900

    성격유형검사 코드 수정

--- algorithm/programmers/level1/성격_유형_검사하기.py
+++ algorithm/programmers/level1/성격_유형_검사하기.py
@@ -1,11 +1,15 @@
 def solution(survey, choices):
-    #rt 몇 점인지
     answer = ''
     scores = dict()
-    for i in range(len(survey)):
-        if survey[i] not in scores:
-            scores[survey[i]] = 0
-        scores[survey[i]] += choices[i] - 4
+    # for i in range(len(survey)):
+    #     if survey[i] not in scores:
+    #         scores[survey[i]] = 0
+    #     scores[survey[i]] += choices[i] - 4
+
+    for survey_type, choice in zip(survey, choices):
+        if survey_type not in scores:
+            scores[survey_type] = 0
+        scores[survey_type] += choice - 4
     def verify_type(type):
         score = scores.get(type, 0) - scores.get(type[::-1], 0)
         if score == 0:
